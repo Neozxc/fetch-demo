1642a783845f668022916c09723c63dae166394f
Lapinas <mrlapinas99@gmail.com>
2022-01-25 10:50:35 +0000

    added fetch method & async await

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './index.css';
 
+// URL: https://cat-fact.herokuapp.com
+
 const App = () => {
+
+  // Method & aSync Await
+  const handleFetch = async () => {
+    const response = await fetch("https://cat-fact.herokuapp.com/facts/random");
+    console.log(response);
+  };
+
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1>Hello API's!</h1>
+      <button onClick={handleFetch}>FETCH ME</button>
     </div>
   );
 }
